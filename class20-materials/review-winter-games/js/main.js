acd81a02bffe33215c907f1061c4d548223224c7
Noel Mac <monforte@terpmail.umd.edu>
2024-02-20 16:56:08 -0500

    added alternate solutions using the filter and map methods

--- class20-materials/review-winter-games/js/main.js
+++ class20-materials/review-winter-games/js/main.js
@@ -10,17 +10,35 @@ function createArray(n){
     return array
 }
 
-function evenArray(arg){
-    let i = 0;
-    let newArray = []
-    arg.forEach((element) =>{            
-        if (element % 2 == 0){
-            newArray[i] = element            
-            i++;
-        }
-    })
-    console.log(newArray)
+function mapArray(arg){
+    // Use the filter method to select elements that fit the below criteria
+    let even = arg.filter(element => element % 2 == 0)
+    // Use the map method to transform the elements through the below callback function
+    let map = even.map(element => element)
+    console.log(map)
 }
+
+// function pushArray(arg){
+//     let push = [];
+//     arg.forEach(element => {
+//         if (element % 2 === 0){
+//             push.push(element)
+//         }
+//     })
+//     console.log(push)
+// }
+
+// function evenArray(arg){
+//     let i = 0;
+//     let newArray = []
+//     arg.forEach((element) =>{            
+//         if (element % 2 == 0){
+//             newArray[i] = element            
+//             i++;
+//         }
+//     })
+//     console.log(newArray)
+// }
 
 // function evenArray(arg){
 //     let evenCounter = 0
