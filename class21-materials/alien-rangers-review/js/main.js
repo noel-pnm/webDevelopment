5179a047d91534941fda839b6cdcc8ebf1b42214
Noel <monforte@terpmail.umd.edu>
2024-02-21 16:32:46 -0500

    test

--- class21-materials/alien-rangers-review/js/main.js
+++ class21-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,34 @@
 //Arrays
 
 //Create and array of tv shows. Loop through and print each show to the console
+function loopTv(){
+    let tvArray = ['show1','show2', 'show3'];
+    tvArray.forEach(element => {console.log(element)})
+}
+
+loopTv()
 
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
 
+function loopEven(){
+    let numberArray = [1,2,3,4,5]
+    let even = numberArray.filter(element => element % 2 == 0)
+    console.log(even)
+}
+
+loopEven()
+
+
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+
+function sumLowHigh(){
+    let arr = [1,2,3,4,5]
+    let sort = arr.sort((a,b) => a-b)
+    let sum = sort[1] - sort[sort.length-1]
+    return sum
+}
+
+sumLowHigh()
+
--- class21-materials/mighty-morphin-review/js/main.js
+++ class21-materials/mighty-morphin-review/js/main.js
@@ -1,18 +1,45 @@
 // *Variables*
 // Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
+let favoriteHoliday = "Christmas"
+favoriteHoliday = "NEW YEAR"
+// toUpperCase() is a method, not a property
+console.log(favoriteHoliday.toUpperCase())
 
 //Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
+let str = "New Year"
+// alert(str.substring(str.length-3,str.length))
+// alert(str.slice(-3))
 
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
-
+function subtractFrom100 (n1, n2, n3, n4, n5){
+    alert(Math.abs(100-n1-n2-n3-n4-n5))
+}
 
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
+function lowHigh(n1,n2,n3){
+    let min = Math.min(n1,n2,n3)
+    let max = Math.max(n1,n2,n3)
+    console.log(`min: ${min}, max: ${max}`)
+    
 
+
+}
 
 // *Conditionals*
 //Create a function that returns heads or tails randomly and as fairly as possible. Call the function.
-
+function HeadTails(){
+    if (Math.random()*100 > 50){
+        console.log("heads")
+    }else {
+        console.log("tails")
+    }
+}
 
 //*Loops*
 //Create a function that takes in a number. Console log the result of heads or tails using the previous function x times where x is the number passed into the function. Call the function.
+function multiFlip(n1){
+    for (let i = 0; i < n1; i++){
+        HeadTails()
+    }
+}
