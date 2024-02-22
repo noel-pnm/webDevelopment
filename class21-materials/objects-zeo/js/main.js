e68adbea5c8afb574bddf41b58ba41eb73e7eda9
Noel <monforte@terpmail.umd.edu>
2024-02-22 12:35:38 -0500

    added class 21/22 materials

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,12 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {
+    // Properties (keys-values)
+    size: 'medium',
+    shape: 'round',
+    model: 'timex',
+    color: 'black',
+    //methods (functions, behaviors)
+    beep: function (){console.log("beep")},
+    boop: function (){console.log("boop")},
+    baap: function (){console.log(this.model)}
+}
