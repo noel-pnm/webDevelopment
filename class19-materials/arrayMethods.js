648d580258d5ac316f947ef271424340b67e9f11
Noel Mac <monforte@terpmail.umd.edu>
2024-02-24 10:45:25 -0500

    completed new questions

--- class19-materials/arrayMethods.js
+++ class19-materials/arrayMethods.js
@@ -10,11 +10,21 @@
 // // camelize("list-style-image") == 'listStyleImage';
 // // camelize("-webkit-transition") == 'WebkitTransition';
 
+strCamelize("background-color")
+
+function strCamelize(str){
+    // convert string into an array, delimited by "-"
+    return str
+        .split('-')
+        .map((element, index) => index == 0 ? element : element[0].toUpperCase() + element.slice(1))
+        .join('')
+}
+
 // /*****************************************************/
 // // Filter range
 // // importance: 4
 // // Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
-
+// return an array where a <= x <= b
 // // The function should not modify the array. It should return the new array.
 
 // // For instance:
@@ -22,58 +32,83 @@
 // // let arr = [5, 3, 8, 1];
 
 // // let filtered = filterRange(arr, 1, 4);
-
 // // alert( filtered ); // 3,1 (matching values)
-
 // // alert( arr ); // 5,3,8,1 (not modified)
-// /*****************************************************/
-// // Filter range "in place"
-// // importance: 4
-// // Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
 
-// // The function should only modify the array. It should not return anything.
+filterMe([5,3,8,1],1,4)
 
-// // For instance:
+function filterMe(arr, a, b){
+    let filter = arr.filter(element => a <= element && element <= b)
+    return `The original array isL ${arr} \nThe filtered array is ${filter}`;
+}
+// The filter() method returns a new array
 
-// // let arr = [5, 3, 8, 1];
+/*****************************************************/
+// Filter range "in place"
+// importance: 4
+// Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. 
+// The test is: a ≤ arr[i] ≤ b.
+// The function should only modify the array. It should not return anything.
+// For instance:
+// let arr = [5, 3, 8, 1];
+// filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+// alert( arr ); // [3, 1]
 
-// // filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+// array.splice(start, deleteCount, addItem1, addItem2)
 
-// // alert( arr ); // [3, 1]
-// /*****************************************************/
-// // Sort in decreasing order
-// // importance: 4
-// // let arr = [5, 2, 1, -10, 8];
+filterRangeInPlace([5,3,8,1,4,2,2,2],1,4)
 
-// // // ... your code to sort it in decreasing order
+function filterRangeInPlace(arr,a,b){
+    arr.forEach((element, index) => {
+        element >= a && element <= b ? "" : arr.splice(index,1);
+    })
+    return arr
+}
 
-// // alert( arr ); // 8, 5, 2, 1, -10
+// Take forEach() method will move through the array, passing the current element and index into the callback function. 
+// When the callbackfn is false, it will delete the current value at the current index from the array.
+
+/*****************************************************/
+// Sort in decreasing order
+// importance: 4
+// let arr = [5, 2, 1, -10, 8];
+// alert( arr ); // 8, 5, 2, 1, -10
+sortDecreasingOrder([5, 2, 1, -10, 8])
+function sortDecreasingOrder(arr){
+    arr.sort((a,b) => (b-a))
+    return arr
+}
+// The function uses the sort method() and apply the known algorithm
+
 // /*****************************************************/
 // Copy and sort array
 // importance: 5
 // We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified.
-
 // Create a function copySorted(arr) that returns such a copy.
-
 // let arr = ["HTML", "JavaScript", "CSS"];
-
 // let sorted = copySorted(arr);
-
 // alert( sorted ); // CSS, HTML, JavaScript
 // alert( arr ); // HTML, JavaScript, CSS (no changes)
+
+copyAndSortArr(["HTML", "JavaScript", "CSS"])
+
+function copyAndSortArr(arr){
+    // Use the spread syntax to create a copy of the array
+    let sorted = arr.slice()
+    // let sorted = [...arr]
+    sorted.sort()
+    return sorted, arr
+}
+
 // /*****************************************************/
 // Create an extendable calculator
 // importance: 5
 // Create a constructor function Calculator that creates “extendable” calculator objects.
 
 // The task consists of two parts.
-
 // First, implement the method calculate(str) that takes a string like "1 + 2" in the format “NUMBER operator NUMBER” (space-delimited) and returns the result. Should understand plus + and minus -.
-
 // Usage example:
-
 // let calc = new Calculator;
-
 // alert( calc.calculate("3 + 7") ); // 10
 // Then add the method addMethod(name, func) that teaches the calculator a new operation. It takes the operator name and the two-argument function func(a,b) that implements it.
 
@@ -89,6 +124,22 @@
 // No parentheses or complex expressions in this task.
 // The numbers and the operator are delimited with exactly one space.
 // There may be error handling if you’d like to add it.
+
+// Object Constructor Function
+function MakeMouse (mouseColor, mouseType, mouseSize, diseasedStatus) {
+    this.color = mouseColor
+    this.type = mouseType
+    this.size = mouseSize
+    this.diseased = diseasedStatus
+    // this.bluetooth = true
+    this.run = function(){}
+    this.hide = function(){}
+    this.eat = function(){}
+}
+
+let mickey = new MakeMouse('Black', 'Cartoon', 'Life Sized', true)
+
+
 // /*****************************************************/
 // Map to names
 // importance: 5
